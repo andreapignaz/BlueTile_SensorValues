af4ee0379172890ca64a324095776cf353349e58
Andrea Pignata <62184044+andreapignaz@users.noreply.github.com>
2022-04-13 17:10:05 +0200

    Update Linux App to match new firmware version

--- LinuxApp/ReadValuesApp.py
+++ LinuxApp/ReadValuesApp.py
@@ -4,7 +4,10 @@ import subprocess
 import time
 
 bt_address = '02:80:E1:00:34:12'
-char_uuid = "00e00000-0001-11e1-ac36-0002a5d5c51b"
+
+env_uuid = "495fb220-bb0d-11ec-bf84-0800200c9a66"
+acc_uuid = "495fb222-bb0d-11ec-bf84-0800200c9a66"
+gyr_uuid = "495fb223-bb0d-11ec-bf84-0800200c9a66"
 
 print("Wait for 5 seconds for the bluetooth scan to complete...")
 proc = subprocess.Popen(["bluetoothctl","scan", "on"],stdout = subprocess.DEVNULL)
@@ -15,22 +18,39 @@ print("Now we can connect!")
 device = BLE_GATT.Central(bt_address)
 # Should add a TRY construct around this :)
 device.connect()
-value = device.char_read(char_uuid)
-print("Random value: " + str(value[0]))
-pressure = 0.0
-pressure = pressure + value[2] + (value[3] << 8) + (value[4] << 16)
-pressure = pressure / 4096.0
-print("Pressure: " + str(pressure) + " hPa")
-humidity = 0.0
-humidity = humidity + value[6] + (value[7] << 8)
-print("Humidity: " + str(humidity))
-temperature = 0.0
-temperature = temperature + value[9] + (value[10] << 8)
-temperature = temperature/10
-print("Temperature: " + str(temperature))
-accX = value[12] + (value[13] << 8)
-accY = value[14] + (value[15] << 8)
-accZ = value[16] + (value[17] << 8)
-print("AccX: " + str(accX) + " AccY: " + str(accY) + " AccZ: " + str(accZ))
+startTime = time.time()
+for x in range(3):
+    value = device.char_read(env_uuid)
+    print("Random value: " + str(value[0]))
+
+    pressure = 0.0
+    pressure = pressure + value[1] + (value[2] << 8) + (value[3] << 16)
+    pressure = pressure / 4096.0
+    print("Pressure: " + str(pressure) + " hPa")
+
+    humidity = 0.0
+    humidity = humidity + value[4] + (value[5] << 8)
+    print("Humidity: " + str(humidity))
+
+    temperature = 0.0
+    temperature = temperature + value[6] + (value[7] << 8)
+    temperature = temperature/10
+    print("Temperature: " + str(temperature))
+
+    value = device.char_read(acc_uuid)
+    accX = value[0] + (value[1] << 8)
+    accY = value[2] + (value[3] << 8)
+    accZ = value[4] + (value[5] << 8)
+    print("AccX: " + str(accX) + " AccY: " + str(accY) + " AccZ: " + str(accZ))
+
+    value = device.char_read(gyr_uuid)
+    gyrX = value[0] + (value[1] << 8)
+    gyrY = value[2] + (value[3] << 8)
+    gyrZ = value[4] + (value[5] << 8)
+    print("GyroX: " + str(gyrX) + " GyroY: " + str(gyrY) + " GyroZ: " + str(gyrZ))
+
+    #print("current Time: ", time.time() - startTime)
+    #20Hz capture:
+    #time.sleep(0.05)
 
 device.disconnect()
